606d98812ab469a7c959c49f11fb64a0be4bf995
IvafaN <46545984+IvafaN@users.noreply.github.com>
2021-03-08 23:24:24 -0800

    first commit

---
+++ 06-practice-class/Category.js
@@ -0,0 +1,27 @@
+// change this Component to a class!
+class Category extends React.Component {
+    constructor() {
+        super()
+    }
+
+    render(){
+
+        return (
+            <div className='row'>
+                <div className='col s2'>
+                    
+                </div>
+            </div>
+        )
+    }
+}
+function Category(props){
+    // get data from props and use map to build an array of <li>
+    // ... code goes here
+    return(
+        <div>
+            {/* Your Code Here */}
+        </div>
+    )
+}
+
